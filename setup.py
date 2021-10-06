0f87e4dc95aba3fbf8b0be86f3f6f467fd299d79
Andy Lu <andy@stitchdata.com>
2021-10-06 16:58:07 -0400

    Merge requirements.txt into setup.py (#6)

--- setup.py
+++ setup.py
@@ -17,9 +17,19 @@ setup(
     py_modules=["tap_nice_incontact"],
     install_requires=[
         "backoff==1.8.0",
+        "certifi==2020.12.5",
+        "chardet==4.0.0",
+        "ciso8601==2.1.3",
+        "idna==2.10",
+        "jsonschema==2.6.0",
+        "python-dateutil==2.8.1",
+        "pytz==2018.4",
         "requests==2.25.1",
+        "simplejson==3.11.1",
         "singer-python==5.12.1",
-        "isodate==0.6.0"
+        "six==1.15.0",
+        "urllib3==1.26.6",
+        "isodate==0.6.0",
     ],
     entry_points="""
     [console_scripts]
